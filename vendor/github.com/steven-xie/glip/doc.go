17c1ccac6f9410cca501569536aa3b9e9e898dad
Steven Xie <dev@stevenxie.me>
2018-09-05 16:31:31 -0400

    Configured Go module, updated CI configs

--- vendor/github.com/steven-xie/glip/doc.go
+++
@@ -1,8 +0,0 @@
-// Package glip provides structs and interfaces for reading and writing to the
-// system clipboard.
-//
-//
-// It is compatible with Windows 7 and onwards (using either PowerShell's
-// "Get-Clipboard" and "Set-Clipboard" cmdlets, or with "clip.exe"), OS X (using
-// "pbcopy" and "pbpaste"), and Linux systems (with "xclip" or "xsel").
-package glip
